c855f5f83ffa593bb1d01b213d375ff1873f887f
harshvi1006 <harshvivaghela05@gmail.com>
2026-05-27 15:51:28 +0530

    Bug fix

--- src/redux/UserSlice.js
+++ src/redux/UserSlice.js
@@ -1,21 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    details: null,
-}
+    details: {},
+};
 
 const userSlice = createSlice({
     name: 'users',
+
     initialState,
+
     reducers: {
-        setUserDetails(state, action) {
-            state.details = action.payload
+        setUserDetails: (state, action) => {
+            state.details = action.payload;
         },
-        clearUserDetails(state) {
-            state.details = null
+
+        clearUserDetails: (state) => {
+            state.details = {};
         },
     },
-})
+});
 
-export const { setUserDetails, clearUserDetails } = userSlice.actions
-export default userSlice.reducer
+export const {
+    setUserDetails,
+    clearUserDetails,
+} = userSlice.actions;
+
+export default userSlice.reducer;
