578a7a0f34485b13e35da2b2cf5d49168221d2e7
harshvi1006 <harshvivaghela05@gmail.com>
2026-06-04 13:47:43 +0530

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ function App() {
                     />
                   }
                 />
-              ))}
+              ))} 
 
               <Route path="*" element={<Navigate to="/dashboard" replace />} />
             </>
